7bf71961340f9c197461ed74d100102687a289b2
최민한 <chmh0411@gmail.com>
2022-08-24 19:37:59 +0900

    초기화면을 SwiftUI로 만들어보았습니다

---
+++ WebSocket-App/SocketView.swift
@@ -0,0 +1,66 @@
+//
+//  SocketView.swift
+//  WebSocket-App
+//
+//  Created by 최민한 on 2022/08/24.
+//
+
+import SwiftUI
+
+struct SocketView: View {
+  @State private var inputText: String = ""
+  
+  var body: some View {
+    VStack {
+      Button {
+      } label: {
+        Text("connect")
+          .foregroundColor(.white)
+          .font(.system(size: 12, weight: .black, design: .monospaced))
+      }
+      .padding(12)
+      .background(Color.blue)
+      .cornerRadius(12)
+      
+      Button {
+      } label: {
+        Text("disconnect")
+          .foregroundColor(.white)
+          .font(.system(size: 12, weight: .black, design: .monospaced))
+      }
+      .padding(12)
+      .background(Color.green)
+      .cornerRadius(12)
+      
+      Divider().padding(.horizontal, 20)
+      
+      TextField("message..", text: $inputText)
+        .font(.system(size: 25, weight: .medium, design: .rounded))
+        .padding(12)
+        .overlay(
+          RoundedRectangle(cornerRadius: 12)
+            .stroke(.black, lineWidth: 2)
+        )
+        .padding(.horizontal, 40)
+      
+      Button {
+        print("DEBUG: send message is \(inputText)")
+        inputText = ""
+      } label: {
+        Text("Send")
+          .font(.system(size: 12, weight: .bold, design: .serif))
+          .foregroundColor(.white)
+      }
+      .padding(12)
+      .background(Color.pink)
+      .cornerRadius(12)
+
+    }
+  }
+}
+
+struct SocketView_Previews: PreviewProvider {
+  static var previews: some View {
+    SocketView()
+  }
+}
